ee03f353ee708c254781085a5d916af247e32337
Alex SinclairJoice <alex.sinclair.joice@gmail.com>
2018-10-05 14:02:05 +0100

    initial commit

---
+++ app/models/game.rb
@@ -0,0 +1,15 @@
+class Game
+
+  def initialize(attributes = {})
+    @id = attributes[:id]
+    @player = attributes[:player]
+    @result = attributes[:result]
+    @move = attributes[:move]
+  end
+
+  def start_game
+
+  end
+
+end
+
