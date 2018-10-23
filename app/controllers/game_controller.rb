de765b75241d54f032cd5018a754f550a2384518
Alex SinclairJoice <alex.sinclair.joice@gmail.com>
2018-10-23 22:20:17 +0100

    begun methods for games controller and added to router

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -5,4 +5,24 @@ class GameController
     @view = GameView.new
   end
 
+  def play
+    mark_position
+  end
+
+  def mark_position
+    positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    position_to_play = @view.get_info("Which position do you want to play?")
+    # if position to play is included in array - remove position from array
+
+    # if positions.include?(position_to_play)
+    #   positions.delete_at(position_to_play - 1)
+    #   p positions
+    # end
+    p positions
+  end
+
+  def check_if_game_over
+
+  end
+
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,14 +1,12 @@
+
 class Game
+  attr_reader :player, :result, :move
 
   def initialize(attributes = {})
     @id = attributes[:id]
     @player = attributes[:player]
     @result = attributes[:result]
     @move = attributes[:move]
-  end
-
-  def start_game
-
   end
 
 end
