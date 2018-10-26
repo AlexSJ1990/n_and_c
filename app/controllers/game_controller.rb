0141eff2d6d2587f918a51b887d3c149015e8a01
Alex SinclairJoice <alex.sinclair.joice@gmail.com>
2018-10-26 14:25:53 +0100

    computer logic for first move complete

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,7 +1,6 @@
 require_relative '../views/game_view'
 
 class GameController
-  # POSITIONS = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
   POSITIONS = {
     "1" => "1",
@@ -20,30 +19,71 @@ class GameController
   end
 
   def play
-    mark_position
+    @starter_player = who_starts
+    @starter_player == "human" ? mark_position : computer_play_first_move
     check_if_game_over
+
+    until @game_over == true
+      if @starter_player == "human"
+        computer_play
+        break if check_if_game_over
+        mark_position
+        break if check_if_game_over
+      else
+        mark_position
+        break if check_if_game_over
+        computer_play
+        break if check_if_game_over
+      end
+    end
   end
+
+
+
+  private
 
   def mark_position
     @view.print_board(POSITIONS)
     position_to_play = @view.get_info("Which position do you want to play?")
     if POSITIONS.keys.include?(position_to_play)
 
-      # in here need a ternary if player not compter ?  x : o
+      # in here need a ternary if player not computer ?  x : o
 
       POSITIONS["#{position_to_play}"] = "x"
     end
-    @view.print_board(POSITIONS)
   end
 
   def check_if_game_over
-    # if positions hash keys include
-    # if positions[1] positions[5] positions[9] == "x, x, x"
-      p [POSITIONS["1"], POSITIONS["5"], POSITIONS["9"]]
-    if [POSITIONS["1"], POSITIONS["5"], POSITIONS["9"]] == ["x", "x", "x"]
-
-    else
-      mark_position
+    if [POSITIONS["1"], POSITIONS["5"], POSITIONS["9"]] == ["x", "x", "x"] ||
+      [POSITIONS["1"], POSITIONS["4"], POSITIONS["7"]] == ["x", "x", "x"] ||
+      [POSITIONS["1"], POSITIONS["2"], POSITIONS["3"]] == ["x", "x", "x"] ||
+      [POSITIONS["3"], POSITIONS["5"], POSITIONS["7"]] == ["x", "x", "x"] ||
+      [POSITIONS["3"], POSITIONS["6"], POSITIONS["9"]] == ["x", "x", "x"] ||
+      [POSITIONS["2"], POSITIONS["5"], POSITIONS["8"]] == ["x", "x", "x"] ||
+      [POSITIONS["7"], POSITIONS["8"], POSITIONS["9"]] == ["x", "x", "x"] ||
+      [POSITIONS["4"], POSITIONS["5"], POSITIONS["6"]] == ["x", "x", "x"]
+      @game_over = true
+      p "Game Over: Winner is "
     end
   end
+
+  def who_starts
+    players = ["human", "computer"]
+    p start = players.sample
+  end
+
+  def computer_play_first_move
+    @view.print_board(POSITIONS)
+    if @starter_player == "computer"
+      start_position = ["1", "3", "5", "7", "9"].sample
+      POSITIONS[start_position] = "[]"
+    elsif @starter_player == "human"
+      p "working on"
+    end
+  end
+
+  def computer_play
+    p "hello"
+  end
+
 end
